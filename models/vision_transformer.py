a1a26757eb6ddee137abd5485d8b431509e094c8
gfrancois <gabriel.francois@ensae.fr>
2025-10-19 20:47:03 +0000

    set up ended with vision_transformer implementation

---
+++ models/vision_transformer.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn as nn 
+
+from patch_embed import PatchEmbedding
+from positional_embeeding import PositionalEmbeeding
+from transformer_encoder import TransformerEncoder
+
+class VisionTransformer(nn.Module):
+    def __init__(self, d_model, n_classes, img_size, patch_size, n_channels, n_heads, n_layers):
+        super().__init__()
+
+        assert img_size[0] % patch_size[0] == 0 and img_size[1] % patch_size[1] == 0, "img_size dimensions must be divisible by patch_size dimensions"
+        assert d_model % n_heads == 0, "d_model must be divisible by n_heads"
+
+        self.d_model = d_model # Dimensionality of model
+        self.n_classes = n_classes # Number of classes
+        self.img_size = img_size # Image size
+        self.patch_size = patch_size # Patch size
+        self.n_channels = n_channels # Number of channels
+        self.n_heads = n_heads # Number of attention heads
+
+        self.n_patches = (self.img_size[0] * self.img_size[1]) // (self.patch_size[0] * self.patch_size[1]) # The number of patches can be found by dividing the product of the height and width of the input image by the product of the height and width of the patch size.
+        self.max_seq_length = self.n_patches + 1
+
+        self.patch_embedding = PatchEmbedding(self.d_model, self.img_size, self.patch_size, self.n_channels)
+        self.positional_encoding = PositionalEmbeeding( self.d_model, self.max_seq_length)
+        self.transformer_encoder = nn.Sequential(*[TransformerEncoder( self.d_model, self.n_heads) for _ in range(n_layers)]) # The vision transformer will also need to be able to have multiple encoder modules. This can be achieved by putting a list of encoder layers inside of a sequential wrapper.
+
+        # Classification MLP
+        self.classifier = nn.Sequential(
+            nn.Linear(self.d_model, self.n_classes),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, images):
+        x = self.patch_embedding(images)
+        x = self.positional_encoding(x)
+        x = self.transformer_encoder(x)
+        x = self.classifier(x[:,0])
+        return x
