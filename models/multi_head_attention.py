ac0d4958aef1fab5c1d48af1750deba2f85d4096
gfrancois <gabriel.francois@ensae.fr>
2025-10-19 16:32:14 +0000

    add attention head and multi attention head

---
+++ models/multi_head_attention.py
@@ -0,0 +1,23 @@
+import torch 
+from torch import nn as nn
+from attention_head  import AttentionHead
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, n_heads):
+        super().__init__()
+        self.head_size = d_model // n_heads
+
+        self.W_o = nn.Linear(d_model, d_model)
+
+        self.heads = nn.ModuleList([AttentionHead(d_model, self.head_size) for _ in range(n_heads)])
+
+    def forward(self, x):
+        # Combine attention heads
+        out = torch.cat([head(x) for head in self.heads], dim=-1)
+        out = self.W_o(out)
+        return out
+
+
+if __name__ == "__main__":
+    print("hi")
+
