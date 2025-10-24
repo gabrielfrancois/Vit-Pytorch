e21ec465a6995ce00d38e947055642fe28c4baf1
leoacpr <leo.leroy@ensae.fr>
2025-10-24 19:42:59 +0000

    load_CIFAR10_data class created

--- data/load_data_leo.py
+++ data/load_data_leo.py
@@ -7,7 +7,7 @@ import numpy as np
 from typing import Tuple
 
 import torch
-from torch.utiles.data import Dataset, DataLoader, random_split #maybe we should avoid random split to make it more reproducible
+from torch.utils.data import Dataset, DataLoader, random_split #maybe we should avoid random split to make it more reproducible
 from torchvision import transforms as T
 
 
@@ -37,4 +37,62 @@ def unpickle(file_path: str) -> dict:
     with open(file_path, "rb") as f: #read binary
         return pickle.load(f, encoding="bytes")
     
+def load_CIFAR10_data(data_dir: str) -> Tuple[DataLoader, DataLoader, DataLoader]:
+    """Loads CIFAR-10 data and returns train / validation / test datasets"""
 
+    #Train data
+    train_batches = [unpickle(os.path.join(data_dir, f"data_batch_{i}")) for i in range(1,5+1)]
+    X_train = np.concatenate([b[b'data'] for b in train_batches]) 
+    y_train = np.concatenate([b[b'labels'] for b in train_batches])
+
+    #Test data
+    test_batch = unpickle(os.path.join(data_dir, "test_batch"))
+    X_test = test_batch[b'data']
+    y_test = np.array(test_batch[b'labels'])
+
+    # Reshape to (N, 3, 32, 32)
+    X_train = X_train.reshape(-1, 3, 32, 32)
+    X_test = X_test.reshape(-1, 3, 32, 32)
+
+    #define transforms according to Gabriel François
+
+    train_transform  = T.Compose([
+        T.ToPILImage(),
+        T.Resize((32, 32)),
+        T.RandomHorizontalFlip(),
+        T.RandAugment(num_ops=2, magnitude=6),
+        T.ToTensor(),
+    ])
+
+    test_transform = T.Compose([ # absolutely no data augmentation here!
+    T.ToPILImage(),
+    T.Resize((32, 32)),
+    T.ToTensor(),
+    ])
+
+    #build datasets
+    full_train_dataset = CIFAR10Datasets(X_train, y_train, transform=train_transform)
+    test_dataset = CIFAR10Datasets(X_test, y_test, transform=test_transform)
+
+    # split full_train_dataset into train and val datasets
+    val_ratio = 0.1 #10% of the data will be used for validation
+    val_size = int(val_ratio * len(full_train_dataset))
+    train_size = len(full_train_dataset) - val_size
+    train_dataset, val_dataset = random_split(
+        full_train_dataset, [train_size, val_size],
+        generator = torch.Generator().manual_seed(42) #reproducibility
+    )
+
+    # Create loaders
+    # maybe adjust the params later!
+    train_loader = DataLoader(train_dataset, batch_size = 64, shuffle=True, num_workers = 2, pin_memory = True)
+    val_loader = DataLoader(val_dataset, batch_size = 64, shuffle = False, num_workers = 2, pin_memory = True)
+    test_loader = DataLoader(test_dataset, batch_size = 64, shuffle = False, num_workers = 2, pin_memory = True)
+
+    print(f"Train: {len(train_dataset)} | Validation: {len(val_dataset)} | Test: {len(test_dataset)}")
+
+    return train_loader, val_loader, test_loader
+
+if __name__ == '__main__':
+    data_dir = "/home/onyxia/work/Vit-Pytorch/data"
+    train_loader, val_loader, test_loader = load_CIFAR10_data(data_dir)
