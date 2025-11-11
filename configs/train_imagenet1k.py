7bd88f3ef39b1a29e81dacad4beaa3c530c0d365
aelbaz <avner3500@gmail.com>
2025-11-11 22:20:21 +0000

    Amélioration des performances GPU avec autocast changement batch size  et autres

--- configs/train_imagenet1k.py
+++ configs/train_imagenet1k.py
@@ -1,12 +1,12 @@
 # train_imagenet1k.py
-d_model = 128        # plus gros que CIFAR (patches + classes)
+d_model = 128        
 n_classes = 1000
 img_size = (128, 128)
 patch_size = (16, 16)
 n_channels = 3
 n_heads = 8
-n_layers = 12
-batch_size = 64          # Risque de ne pas tenir si les batchs sont trop gros
-epochs = 3
+n_layers = 7 
+batch_size = 256          
+epochs = 1
 alpha = 0.0005
 weight_decay = 1e-4
