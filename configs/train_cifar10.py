565ff387887e5ddee23452e15d8cbbb02da89565
aelbaz <avner3500@gmail.com>
2025-12-14 17:31:56 +0000

    modification_bis

--- configs/train_cifar10.py
+++ configs/train_cifar10.py
@@ -1,11 +1,11 @@
 # setting up the hyperparametters
 
 d_model = 32  # 12 before
-n_classes = 10
-img_size = (32, 32)
-patch_size = (4, 4)
-n_channels = 3  # before it was 1
-n_heads = 4
+n_classes = 100
+img_size = (32,32)
+patch_size = (4,4)
+n_channels = 3 # before it was 1
+n_heads = 4 
 n_layers = 12
 batch_size = 100
 epochs = 100
@@ -20,14 +20,14 @@ data_dir = "/home/onyxia/work/Vit-Pytorch/data"
 
 plot_dir = "plots/plots_CIFAR"
 
-checkpoint_dir = "checkpoints"
+checkpoint_dir = "checkpoints/checkpoint_CIFAR"
 
 # other
 
 resume = True  # permet soit de continuer à partir du checkpoint enregistré soit de repartir de 0
 
-pruning_index = [4, 7, 10]
+pruning_index = [4,7,10]
 rho = 0.7
-lambda_kl = 1 / 2
+lambda_kl = 1/2
 lambda_ratio = 2
-lambda_distill = 1 / 2
+lambda_distill = 1/2
